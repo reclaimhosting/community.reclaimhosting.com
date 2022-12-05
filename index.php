a17421b3fafc226bb4d88ee1b59b7f2b6ea90488
Taylor Jadin <jadin.taylor@gmail.com>
2022-12-05 20:50:18 +0000

    current year in footer using js

--- index.php
+++ index.php
@@ -62,8 +62,11 @@
       <div class="divider bottom-divider"></div>
       <div class="footer container-fluid bd-gutter flex-wrap flex-lg-nowrap">
         <a href="https://reclaimhosting.com">reclaimhosting.com</a>
-        <p>© All Rights Reserved 2022</p>
-      </div>      
+        <p>© All Rights Reserved <span id="current-year"></span></p>
+      </div>
+      <script>
+        document.getElementById("current-year").innerHTML = new Date().getFullYear();
+      </script>
 
       	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
